d6aa45db9a1c66b6ad7df2834dc373ec31267f05
Elena Pavlova <lenlho22@gmail.com>
2022-04-07 09:15:11 +0300

    fetch users with createAsyncThunk from @reduxjs/toolkit

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -1,23 +1,12 @@
 import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import { AppDispatch } from "../store";
 import { IUser } from "../../models/IUser";
-import { userSlice } from './UserSlice';
 
-const {usersFetching, usersFetchingError, usersFetchingSuccess} = userSlice.actions;
-
-// Classic way for getting data scenarios with Thunk
-export const fetchUsers = () => async (dispatch: AppDispatch) =>{
-    try {
-        dispatch(usersFetching());
+export const fetchUsers = createAsyncThunk(
+    'user/fetchAll',
+    async (_, thunkAPI) => {
         const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
-        dispatch(usersFetchingSuccess(response.data));
-    } catch(error) {
-        // const typedError = error as Error;
-        // dispatch(usersFetchingError(typedError?.message));
-
-        if (axios.isAxiosError(error)) {
-            dispatch(usersFetchingError(error.message));
-        }
-    }
-};
+        return response.data;
+    },
+);
